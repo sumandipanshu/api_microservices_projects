430b1b74283b1b9d407e6b916f8e4abf39a0b126
Dipanshu Kumar Suman <dipanshukumarsuman@gmail.com>
2020-09-30 22:25:19 +0530

    created Users model for mongodb and added post route to '/api/exercise/new-user'

--- exercise_tracker/index.js
+++ exercise_tracker/index.js
@@ -20,6 +20,74 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const { Schema } = mongoose;
+var Exercise = mongoose.model(
+  "Exercise",
+  new Schema({
+    description: { type: String, required: true },
+    duration: { type: Number, required: true, min: 1 },
+    date: Date,
+  })
+);
+
+var Users = mongoose.model(
+  "Users",
+  new Schema(
+    {
+      username: String,
+      count: Number,
+      log: [
+        new Schema(
+          {
+            description: String,
+            duration: Number,
+            date: { type: Date, get: getDate },
+          },
+          {
+            toObject: { getters: true },
+            toJSON: { getters: true },
+          }
+        ),
+      ],
+    },
+    {
+      toObject: { getters: true },
+      toJSON: { getters: true },
+    }
+  )
+);
+
+function getDate(date) {
+  return date.toDateString();
+}
+
+app.post(
+  "/api/exercise/new-user",
+  function (req, res, next) {
+    var username = req.body.username.trim();
+    if (!username) {
+      res.send("Path `username` is required.");
+    } else {
+      Users.findOne({ username: username }, function (err, data) {
+        if (!data) {
+          var user = Users({ username: username });
+          user.save(function (err) {
+            if (err) res.send(err);
+            next();
+          });
+        } else {
+          res.send("Username already taken");
+        }
+      });
+    }
+  },
+  function (req, res) {
+    Users.findOne({ username: req.body.username }, function (err, data) {
+      res.json({ _id: data._id, username: data.username });
+    });
+  }
+);
+
 // Not found middleware
 app.use((req, res, next) => {
   return next({ status: 404, message: "not found" });
